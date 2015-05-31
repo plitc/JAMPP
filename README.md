c751b47b817a9a09e0acfcfcfeaf37a0c52beea0
Daniel Plominski <Daniel@Plominski.eu>
2015-05-31 22:53:22 +0200

    prototype 0.1

---
+++ README.md
@@ -0,0 +1,89 @@
+
+LICENSE
+=======
+* BSD 2-Clause
+
+Background
+==========
+* XAMPP Apache + MySQL + PHP + Perl for FreeBSD Jails
+
+Benefits of Goal Setting
+========================
+* XAMPP Apache + MySQL + PHP + Perl for FreeBSD Jails
+
+WARNING
+=======
+* jampp is experimental and its not ready for production. Do it at your own risk.
+
+Dependencies
+============
+* FreeBSD
+   * Jail (Support)
+
+* free hard disk space requirement: 2-3 GB
+
+HOST (Package) Dependencies
+=================
+```
+```
+
+Features
+========
+* install
+
+* start
+
+* stop
+
+* uninstall
+
+usage:
+```
+```
+
+Platform
+========
+* FreeBSD
+   * 10+
+
+Usage
+=====
+```
+    WARNING: jampp is experimental and its not ready for production. Do it at your own risk.
+
+    # usage: ./jampp.sh { install | start | stop | uninstall }
+```
+
+Example
+=======
+* install
+```
+```
+
+* start
+```
+```
+
+* stop
+```
+```
+
+* uninstall
+```
+```
+
+Diagram
+=======
+
+Screencast
+==========
+
+Errata
+======
+* 31.05.2015:
+```
+
+TODO
+====
+* 31.05.2015:
+
---
+++ jampp.sh
@@ -0,0 +1,266 @@
+#!/bin/sh
+
+### LICENSE - (BSD 2-Clause) // ###
+#
+# Copyright (c) 2015, Daniel Plominski (Plominski IT Consulting)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice, this
+# list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+### // LICENSE - (BSD 2-Clause) ###
+
+### ### ### PLITC // ### ### ###
+
+### stage0 // ###
+OSVERSION=$(uname)
+MYNAME=$(whoami)
+#
+PRG="$0"
+##/ need this for relative symlinks
+while [ -h "$PRG" ] ;
+do
+   PRG=$(readlink "$PRG")
+done
+DIR=$(dirname "$PRG")
+#
+ADIR="$PWD"
+#
+#/ spinner
+spinner()
+{
+   local pid=$1
+   local delay=0.01
+   local spinstr='|/-\'
+   while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+         local temp=${spinstr#?}
+         printf " [%c]  " "$spinstr"
+         local spinstr=$temp${spinstr%"$temp"}
+         sleep $delay
+         printf "\b\b\b\b\b\b"
+   done
+   printf "    \b\b\b\b"
+}
+#
+#/ function cleanup tmp
+cleanup(){
+   rm -rf /etc/jampp/tmp/*
+}
+### // stage0 ###
+
+case "$1" in
+'install')
+### stage1 // ###
+case $OSVERSION in
+FreeBSD)
+### stage2 // ###
+#
+### // stage2 ###
+
+### stage3 // ###
+if [ "$MYNAME" = "root" ]; then
+   : # dummy
+else
+   : # dummy
+   : # dummy
+   echo "[ERROR] You must be root to run this script"
+   exit 1
+fi
+### stage4 // ###
+#
+### ### ### ### ### ### ### ### ###
+
+
+
+### ### ### ### ### ### ### ### ###
+#/ cleanup
+### ### ### ### ### ### ### ### ###
+echo "" # printf
+printf "\033[1;31mjampp installation finished.\033[0m\n"
+### ### ### ### ### ### ### ### ###
+#
+### // stage4 ###
+#
+### // stage3 ###
+#
+### // stage2 ###
+   ;;
+*)
+   # error 1
+   : # dummy
+   : # dummy
+   echo "[ERROR] Plattform = unknown"
+   exit 1
+   ;;
+esac
+#
+### // stage1 ###
+;;
+'start')
+### stage1 // ###
+case $OSVERSION in
+FreeBSD)
+### stage2 // ###
+#
+### // stage2 ###
+
+### stage3 // ###
+if [ "$MYNAME" = "root" ]; then
+   : # dummy
+else
+   : # dummy
+   : # dummy
+   echo "[ERROR] You must be root to run this script"
+   exit 1
+fi
+### stage4 // ###
+#
+### ### ### ### ### ### ### ### ###
+
+
+
+### ### ### ### ### ### ### ### ###
+#/ cleanup
+### ### ### ### ### ### ### ### ###
+echo "" # printf
+printf "\033[1;31mjampp start finished.\033[0m\n"
+### ### ### ### ### ### ### ### ###
+#
+### // stage4 ###
+#
+### // stage3 ###
+#
+### // stage2 ###
+   ;;
+*)
+   # error 1
+   : # dummy
+   : # dummy
+   echo "[ERROR] Plattform = unknown"
+   exit 1
+   ;;
+esac
+#
+### // stage1 ###
+;;
+'stop')
+### stage1 // ###
+case $OSVERSION in
+FreeBSD)
+### stage2 // ###
+#
+### // stage2 ###
+
+### stage3 // ###
+if [ "$MYNAME" = "root" ]; then
+   : # dummy
+else
+   : # dummy
+   : # dummy
+   echo "[ERROR] You must be root to run this script"
+   exit 1
+fi
+### stage4 // ###
+#
+### ### ### ### ### ### ### ### ###
+
+
+
+### ### ### ### ### ### ### ### ###
+#/ cleanup
+### ### ### ### ### ### ### ### ###
+echo "" # printf
+printf "\033[1;31mjampp stop finished.\033[0m\n"
+### ### ### ### ### ### ### ### ###
+#
+### // stage4 ###
+#
+### // stage3 ###
+#
+### // stage2 ###
+   ;;
+*)
+   # error 1
+   : # dummy
+   : # dummy
+   echo "[ERROR] Plattform = unknown"
+   exit 1
+   ;;
+esac
+#
+### // stage1 ###
+;;
+'uninstall')
+### stage1 // ###
+case $OSVERSION in
+FreeBSD)
+### stage2 // ###
+#
+### // stage2 ###
+
+### stage3 // ###
+if [ "$MYNAME" = "root" ]; then
+   : # dummy
+else
+   : # dummy
+   : # dummy
+   echo "[ERROR] You must be root to run this script"
+   exit 1
+fi
+### stage4 // ###
+#
+### ### ### ### ### ### ### ### ###
+
+
+
+### ### ### ### ### ### ### ### ###
+#/ cleanup
+### ### ### ### ### ### ### ### ###
+echo "" # printf
+printf "\033[1;31mjampp uninstallation finished.\033[0m\n"
+### ### ### ### ### ### ### ### ###
+#
+### // stage4 ###
+#
+### // stage3 ###
+#
+### // stage2 ###
+   ;;
+*)
+   # error 1
+   : # dummy
+   : # dummy
+   echo "[ERROR] Plattform = unknown"
+   exit 1
+   ;;
+esac
+#
+### // stage1 ###
+;;
+*)
+printf "\033[1;31mWARNING: jampp is experimental and its not ready for production. Do it at your own risk.\033[0m\n"
+echo "" # usage
+echo "usage: $0 { install | start | stop | uninstall }"
+;;
+esac
+exit 0
+### ### ### PLITC ### ### ###
+# EOF
