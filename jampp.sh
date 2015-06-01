a1880940408a8847301ec6b673f369f5f758ef0a
Daniel Plominski <Daniel@Plominski.eu>
2015-06-01 05:14:25 +0200

    prototype 0.1.3.6

--- jampp.sh
+++ jampp.sh
@@ -278,7 +278,7 @@ APACHEPHP
       echo "[ERROR] php connection failed"
       exit 1
    fi
-   if [ "$CHECKPHPTEST" -gt = "0" ]; then
+   if [ "$CHECKPHPTEST" -gt 0 ]; then
       echo "PHP TEST passed"
    fi
    rm -f usr/local/www/apache24/data/phptest
